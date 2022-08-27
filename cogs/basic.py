8030ef5d3ddc2c2c0a2336884c1b97702b0809e9
Developer X <computer.tech.yt@gmail.com>
2022-08-27 18:25:12 +0500

    Add files via upload

---
+++ cogs/basic.py
@@ -0,0 +1,74 @@
+from math import fabs
+import discord
+import datetime, time
+from discord import app_commands
+from discord.ext import commands
+import config
+
+startTime = time.time()
+
+class Basic(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @app_commands.command(name="ping", description="Show's bot latency")
+    async def ping(self, interaction: discord.Interaction):
+        await interaction.response.defer(ephemeral=True)
+        latency = round(self.bot.latency * 1000)
+
+        if latency < 150:
+            if latency < 75:
+                await interaction.followup.send(f"<:latency:954610357647851550> **Bot Latency:** <:signal5:954624490132873256> `{latency}`ms", ephemeral=True)
+                return
+            else:
+                await interaction.followup.send(f"<:latency:954610357647851550> **Bot Latency:** <:signal4:954624489570844692>`{latency}`ms", ephemeral=True)
+                return
+
+        if latency > 150:
+            if latency > 300:
+                await interaction.followup.send(f"<:latency:954610357647851550> **Bot Latency:** <:signal1:954624489197559828>`{latency}`ms", ephemeral=True)
+                return
+            if latency > 225:
+                await interaction.followup.send(f"<:latency:954610357647851550> **Bot Latency:** <:signal2:954624489340153906>`{latency}`ms", ephemeral=True)
+                return
+            else:
+                await interaction.followup.send(f"<:latency:954610357647851550> **Bot Latency:** <:signal3:954624491558936596>`{latency}`ms", ephemeral=True)
+                return
+
+    @app_commands.command(name="info", description="Show's information about the bot")
+    async def info(self, interaction: discord.Interaction):
+        botinfo = await self.bot.application_info()
+        count = 0
+        for guild in self.bot.guilds:
+            count += guild.member_count
+
+        info_embed = discord.Embed(
+            title="Cleaner#8788",
+            description="**An Advanced Cleaning Utility**",
+            color=discord.Color.magenta()
+        )
+        info_embed.set_thumbnail(url="https://i.imgur.com/T12D7JH.png")
+        info_embed.add_field(name="Founder of Cleaner", value=botinfo.owner, inline=True)
+        info_embed.add_field(name="Developers", value=f"{botinfo.owner}\nsarth#0460", inline=True)
+        info_embed.add_field(name="Beta Testers", value=f"RVG|𝓵𝓸𝓻𝔂#9995\nGod Of Life#2140\nTomClancy#0575", inline=True)
+        info_embed.add_field(name="Graphics by", value="Freepik at FlatIcon:\nhttps://www.flaticon.com/authors/freepik", inline=False)
+        info_embed.add_field(name="Development Language", value="Python", inline=True)
+        info_embed.add_field(name="Host", value="Creavite.co | Pterodactyl Panel", inline=True)
+        info_embed.add_field(name="Uptime", value=str(datetime.timedelta(seconds=int(round(time.time()-startTime)))), inline=True)
+        info_embed.add_field(name="Ping", value=f"{round(self.bot.latency * 1000)} ms", inline=True)
+        info_embed.add_field(name="Language", value="EN-US", inline=True)
+        info_embed.add_field(name="Version", value=f"v{config.BOT_VERSION}", inline=True)
+        info_embed.add_field(name="Guild Count", value=len(self.bot.guilds), inline=True)
+        info_embed.add_field(name="Member Count", value=count, inline=True)
+        info_embed.add_field(name="Command Count", value=25, inline=True)
+        info_embed.add_field(name="Bot Invite", value="http://dsc.gg/ccleaner", inline=True)
+        info_embed.add_field(name="Support Server", value="https://dsc.gg/cleaner-support", inline=True)
+        info_embed.set_footer(text="Chech out latest updates! do /changelog")
+
+        await interaction.response.send_message(embed=info_embed, ephemeral=True)
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(
+        Basic(bot))
+
+#self.bot.application_info()
---
+++ cogs/delete.py
@@ -0,0 +1,136 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+from discord.app_commands.errors import MissingPermissions, BotMissingPermissions
+from discord.app_commands.checks import has_permissions
+
+class Delete(commands.Cog):
+    def __init__(self, bot:commands.Bot) -> None:
+        self.bot = bot
+
+    delete_group = app_commands.Group(name="delete", description="Delete unnecassry channel or roles")
+
+    @delete_group.command(name="channel", description="Delete unnecessary channels")
+    @app_commands.describe(channel="The channel you want to delete.")
+    @has_permissions(manage_channels=True)
+    async def channel(self, interaction: discord.Interaction, channel: discord.TextChannel):
+        await interaction.response.defer(ephemeral=True)
+        try:
+            await channel.delete()
+            await interaction.followup.send(f"<:clean:954611061577896006> Deleted **#{channel}**")
+        except discord.errors.Forbidden:
+            await interaction.followup.send(f"<:error:954610357761105980> Sorry {interaction.user.mention}, I do not have the required **(Manage Channels)** permissions to do that!")
+        
+    @channel.error
+    async def channel_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, MissingPermissions):
+            await interaction.response.send_message(f"<:error:954610357761105980> Sorry {interaction.user.mention}, you do not have the required **(Manage Channels)** permissions to do that!", ephemeral=True)
+        
+    @delete_group.command(name="thread", description="Delete unnecessary thread")
+    @app_commands.describe(thread="The thread you want to delete.")
+    @has_permissions(manage_threads=True)
+    async def thread(self, interaction: discord.Interaction, thread: discord.Thread):
+        await interaction.response.defer(ephemeral=True)
+        try:
+            await thread.delete()
+            await interaction.followup.send(f"<:clean:954611061577896006> Deleted **#{thread}**")
+        except discord.errors.Forbidden:
+            await interaction.followup.send(f"<:error:954610357761105980> Sorry {interaction.user.mention}, I do not have the required **(Manage Threads)** permissions to do that!")
+        
+    @thread.error
+    async def thread_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, MissingPermissions):
+            await interaction.response.send_message(f"<:error:954610357761105980> Sorry {interaction.user.mention}, you do not have the required **(Manage Threads)** permissions to do that!", ephemeral=True)
+        else:
+            raise Exception
+
+    @delete_group.command(name="role", description="Delete unnecessary roles")
+    @app_commands.describe(role="The role you want to delete.")
+    @has_permissions(manage_roles=True)
+    async def role(self, interaction: discord.Interaction, role: discord.Role):
+        await interaction.response.defer(ephemeral=True)
+        await role.delete()
+        await interaction.followup.send(f"<:clean:954611061577896006> Deleted **{role}**")
+        await interaction.followup.send(f"<:error:954610357761105980> Sorry {interaction.user.mention}, I do not have the required **(Manage Roles)** permissions to do that!")
+        
+    @role.error
+    async def role_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, MissingPermissions):
+            await interaction.response.send_message(f"<:error:954610357761105980> Sorry {interaction.user.mention}, you do not have the required **(Manage Roles)** permissions to do that!", ephemeral=True)
+        else:
+            raise Exception
+
+    @delete_group.command(name="nickname", description="Delete/Reset someone's nickname to their default username.")
+    @app_commands.describe(user="The user who's nickname you want to delete.")
+    @has_permissions(manage_nicknames=True)
+    async def nickname(self, interaction: discord.Interaction, user: discord.Member):
+        nick = user.display_name
+        name = user.name
+        if nick == name:
+            await interaction.response.send_message(f"<:warn:954610357748510770> Oops! {user.mention} doesn't have any nickname.", ephemeral=True)
+        if nick != name:
+            await user.edit(nick=None)
+            await interaction.response.send_message(f"<:done:954610357727543346> Success! {user.mention}'s nickname has been removed.", ephemeral=True)
+        
+    @nickname.error
+    async def nick_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, MissingPermissions):
+            await interaction.response.send_message(embed=discord.Embed(description="<:error:954610357761105980> Missing Permissions, you are missing (Manage Nicknames) permission to invoke this command.", color=discord.Color.red()), ephemeral=True)
+        if isinstance(error, BotMissingPermissions):
+            await interaction.response.send_message(embed=discord.Embed(description="<:error:954610357761105980> Missing Permissions, I'm missing (Manage Nicknames) permission to process this command.", color=discord.Color.red()), ephemeral=True)
+        else:
+            raise Exception
+    
+    @delete_group.command(name="emoji", description="Delete unwanted emojis from the server.")
+    @app_commands.describe(emoji="The emoji you want to delete.")
+    @has_permissions(manage_emojis=True)
+    async def emoji(self, interaction: discord.Interaction, emoji: str):
+        try:
+            emoji_id = discord.PartialEmoji.from_str(emoji).id
+            if interaction.client.get_emoji(emoji_id).guild.id == interaction.guild.id:
+                emoji = interaction.client.get_emoji(emoji_id)
+                await emoji.delete()
+                await interaction.response.send_message("<:done:954610357727543346> Success! The specified emoji has been deleted.", ephemeral=True)
+            
+            if interaction.client.get_emoji(emoji_id).guild.id != interaction.guild.id:
+                await interaction.response.send_message("<:error:954610357761105980> Oops! The specified emoji doesn't belong to this server/guild.", ephemeral=True)
+        except:
+            await interaction.response.send_message("<:error:954610357761105980> Oops! I can't locate that emoji.", ephemeral=True)
+    
+    @emoji.error
+    async def emoji_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, MissingPermissions):
+            await interaction.response.send_message(embed=discord.Embed(description="<:error:954610357761105980> Missing Permissions, you are missing (Manage Emojis and Stickers) permission to invoke this command.", color=discord.Color.red()), ephemeral=True)
+        if isinstance(error, BotMissingPermissions):
+            await interaction.response.send_message(embed=discord.Embed(description="<:error:954610357761105980> Missing Permissions, I'm missing (Manage Emojis and Stickers) permission to process this command.", color=discord.Color.red()), ephemeral=True)
+        else:
+            raise Exception
+
+    @delete_group.command(name="category", description="Delete unwanted categories including their channels.")
+    @app_commands.describe(category="The category you want to delete.")
+    @has_permissions(manage_channels=True)
+    async def category(self, interaction: discord.Interaction, category: discord.CategoryChannel):
+        await interaction.response.defer(ephemeral=True)
+        index = 0
+        channel_names = ""
+        for channel in category.channels:
+            index += 1
+            channel_names += f"{index}. {channel.name}\n"
+            await channel.delete(reason="Invoked Category Delete Command")
+        await category.delete()
+
+        embed = discord.Embed(description=f"<:done:954610357727543346> {category.name} has been deleted with following channels:\n{channel_names}", color=discord.Color.green())
+        await interaction.followup.send(embed=embed)
+
+    @category.error
+    async def category_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, MissingPermissions):
+            await interaction.response.send_message(f"<:error:954610357761105980> Sorry {interaction.user.mention}, you do not have the required **(Manage Channels)** permissions to do that!", ephemeral=True)
+        if isinstance(error, BotMissingPermissions):
+            await interaction.response.send_message(embed=discord.Embed(description="<:error:954610357761105980> Missing Permissions, I'm missing (Manage Channels) permission to process this command.", color=discord.Color.red()), ephemeral=True)
+
+
+
+async def setup(bot: commands.Cog) -> None:
+    await bot.add_cog(
+        Delete(bot))
